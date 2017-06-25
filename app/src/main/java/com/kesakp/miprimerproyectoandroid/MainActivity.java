9769de83486b6ca1a0f592f0ae8e772dfbfa499c
kesakp <pihyknup@hotmail.com>
2017-06-25 17:42:57 -0500

    Add files via upload

---
+++ app/src/main/java/com/kesakp/miprimerproyectoandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kesakp.miprimerproyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
